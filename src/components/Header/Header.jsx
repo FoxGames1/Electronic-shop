bba2c8fa4adfe66b018bad147515271ffc1c5c1f
Ivan Makarevich <ivanmakarevic3@gmail.com>
2024-11-29 17:40:47 +0300

    Сделал начало для hederа

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../css/Header/Header.module.css'
+
+const Header = () => {
+	return (
+		<>
+			<header className="header">
+				<div className="header__top">
+					<div className="header__top-line"></div>
+					<div className="header__top-menu">
+						
+					</div>
+				</div>	
+			</header>		
+		</>
+	);
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './css/reset.css'
+
 import App from './App.jsx'
+
+import './css/reset.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
