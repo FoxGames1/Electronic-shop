7466a823e5de6f8faea9601c2491d135dd7c2af7
Ivan <ivanmakarevic3@gmail.com>
2024-12-02 01:45:07 +0300

    Работа с шапкой и API antd

--- src/components/Header/Reg.jsx
+++ src/components/Header/Reg.jsx
@@ -1,28 +1,53 @@
 import styles from '../../css/Header/Reg.module.css';
-import { Select } from "antd";
+import { Select, ConfigProvider } from "antd";
+import { ReactComponent as LocationIcon } from '../../../public/Images/Header/Header__top/btn/locationIcon.svg';
 
-// import locationIcon from '/Images/Header/Header__top/btn/locationIcon.svg';
-// import locationArrow from '/Images/Header/Header__top/btn/locationIcon_arrow.svg';
-// import registrationIcon from '/Images/Header/Header__top/btn/log in.svg';
 const handleChange = (e) => {
     console.log(`selected ${e}`);
-  };
-const Reg = () => {
+}
 
+
+
+
+const Reg = () => {
     return (
-        <div className={styles.register} >
-        <Select 
-            className={styles.dropdown}
-            defaultValue="Москва"
-            onChange={handleChange}
-            options={[
-                { value: 'москва', label: 'Москва' },
-                { value: 'москва', label: 'Москва' },
-                { value: 'москва', label: 'Москва' }
-            ]}
+        <ConfigProvider
+          theme={{
+            components: {
+              Select: {
+                multipleSelectorBgDisabled: 'var(--light-green)',
+                optionSelectedBg: 'var(--light-green)',
+                optionActiveBg: 'var(--light-green)',
+                colorPrimary: 'var(--dark-grey)',
+                colorBorder: 'none',
+                activeOutlineColor: 'var(--light-green)',
+                boxShadow: 'none',
+              },
+            },
+          }}
         >
-        </Select>
+        <div className={styles.register}>
+            <Select
+                className={styles.dropdown}
+                defaultValue="Москва"
+                onChange={handleChange}
+                options={[
+                    { value: 'москва', label: 'Москва' },
+                    { value: 'санкт-петербург', label: 'Санкт-Петербург' },
+                    { value: 'казань', label: 'Казань' }
+                ]}
+                prefix={<LocationIcon />}
+                style={
+                    {
+                        width: 120, 
+                        textAlign: 'center', 
+                        alignItems: 'center',
+                    }
+                }
+                
+            />
         </div>
+        </ConfigProvider>
     );
 };
 
